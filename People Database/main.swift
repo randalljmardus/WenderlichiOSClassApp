be61645c47b6f8cae8ce531dc11c9b4537d794a4
Randall Mardus <randalljmardus@gmail.com>
2016-03-16 12:52:24 -0400

    Initial Commit

---
+++ People Database/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  People Database
+//
+//  Created by Randall Mardus on 3/16/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
